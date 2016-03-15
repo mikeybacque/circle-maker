38422bbe72f311ba7bba08630f77643caf17a2d7
mikeybacque <mikey.bacque@gmail.com>
2016-03-15 16:45:42 -0400

    add javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $html = $('html');
+var $body = $('body');
+
+$html.on('keydown', function (e) {
+  var div = $('<div>');
+
+  $body.append(div);
+  div.addClass('ball');
+  div.css('top', Math.random() * 1000);
+  div.css('left', Math.random() * 1000);
+  div.css('width', Math.random() * 100);
+  div.css('height', Math.random() * 100);
+});
